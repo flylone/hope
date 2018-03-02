2e342e9a85244b57d470ae91c20ec879e485354d
wushouqiang <wushouqiang@idealens.com>
2018-03-02 15:53:43 +0800

    init commit

---
+++ hope-web/src/main/java/com/wsq/hope/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.wsq.hope.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(ModelMap map) {
+        map.put("world", "world");
+        return "home/index";
+    }
+}
